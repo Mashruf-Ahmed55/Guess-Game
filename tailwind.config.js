dbd2cb144d2ea7dade3b20dea21274e526c25463
Mashruf-Ahmed55 <mashrufahmed03@gmail.com>
2024-07-16 21:12:09 +0600

    htmlupdate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}'
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "bx": '0 3px 10px rgb(0, 0, 0, 0.2)'
+      }
+    },
   },
   plugins: [],
 }
